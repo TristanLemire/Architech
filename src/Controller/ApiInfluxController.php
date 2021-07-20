667a7c39d57afba18b4c3d66423c06b78193da10
rorrotapia <rodrigo.tapiabravo@gmail.com>
2021-07-20 12:19:57 +0200

    update data influx + new format

--- src/Controller/ApiInfluxController.php
+++ src/Controller/ApiInfluxController.php
@@ -11,35 +11,51 @@ use InfluxDB2\Client;
 use InfluxDB2\Model\WritePrecision;
 use InfluxDB2\Point;
 
+const SENSOR_TYPE = [
+  'Temperature' => 112,
+  'Humidité' => 114,
+  'Pression' => 116
+];
+
 class ApiInfluxController extends AbstractController
 {
-    /**
-     * @Route("/api/influx", name="api_influx")
-     */
-    public function index(JsonMessage $jsonMessage): Response
-    {
-        $response = array();
-        # You can generate a Token from the "Tokens Tab" in the UI
-        $token = 'ibJqsWzlGD-lgMSSTuYOcG2iGybuD6sz8Famee00YhzQvEagUO_ULKjb94SrL-XzbLCbLsNAk_JZTgqscY_OUA==';
-        $org = 'tristan.lemire@hetic.net';
-        $bucket = "tristan.lemire's Bucket";
+  /**
+   * @Route("/api/influx", name="api_influx")
+   */
+  public function index(JsonMessage $jsonMessage): Response
+  {
+    $response = array();
+    # You can generate a Token from the "Tokens Tab" in the UI
+    $token = 'ibJqsWzlGD-lgMSSTuYOcG2iGybuD6sz8Famee00YhzQvEagUO_ULKjb94SrL-XzbLCbLsNAk_JZTgqscY_OUA==';
+    $org = 'tristan.lemire@hetic.net';
+    $bucket = "tristan.lemire's Bucket";
 
-        $client = new Client([
-            "url" => "https://eu-central-1-1.aws.cloud2.influxdata.com",
-            "token" => $token,
-            "bucket" => $bucket,
-            'org' => $org
-        ]);
+    $client = new Client([
+      "url" => "https://eu-central-1-1.aws.cloud2.influxdata.com",
+      "token" => $token,
+      "bucket" => $bucket,
+      'org' => $org
+    ]);
 
-        $query = 'from(bucket: "'.$bucket.'")
+    $query = 'from(bucket: "'.$bucket.'")
   |> range(start: -5m)
-  |> filter(fn: (r) => r["topic"] == "WEB3-GROUPE8/042101/116" or r["topic"] == "WEB3-GROUPE8/042102/112" or r["topic"] == "WEB3-GROUPE8/042102/114" or r["topic"] == "WEB3-GROUPE8/042102/116" or r["topic"] == "WEB3-GROUPE8/042103/114" or r["topic"] == "WEB3-GROUPE8/042103/112" or r["topic"] == "WEB3-GROUPE8/042103/116" or r["topic"] == "WEB3-GROUPE8/042104/112" or r["topic"] == "WEB3-GROUPE8/042104/114" or r["topic"] == "WEB3-GROUPE8/042104/116" or r["topic"] == "WEB3-GROUPE8/042105/112" or r["topic"] == "WEB3-GROUPE8/042105/114" or r["topic"] == "WEB3-GROUPE8/042105/116" or r["topic"] == "WEB3-GROUPE8/042106/112" or r["topic"] == "WEB3-GROUPE8/042106/114" or r["topic"] == "WEB3-GROUPE8/042106/116" or r["topic"] == "WEB3-GROUPE8/042107/112" or r["topic"] == "WEB3-GROUPE8/042107/116" or r["topic"] == "WEB3-GROUPE8/042107/114" or r["topic"] == "WEB3-GROUPE8/042108/112" or r["topic"] == "WEB3-GROUPE8/042108/114" or r["topic"] == "WEB3-GROUPE8/042108/116" or r["topic"] == "WEB3-GROUPE8/042109/112" or r["topic"] == "WEB3-GROUPE8/042109/114" or r["topic"] == "WEB3-GROUPE8/042109/116" or r["topic"] == "WEB3-GROUPE8/042110/112" or r["topic"] == "WEB3-GROUPE8/042110/114" or r["topic"] == "WEB3-GROUPE8/042202/112" or r["topic"] == "WEB3-GROUPE8/042110/116" or r["topic"] == "WEB3-GROUPE8/042202/114" or r["topic"] == "WEB3-GROUPE8/042202/116" or r["topic"] == "WEB3-GROUPE8/042204/112" or r["topic"] == "WEB3-GROUPE8/042204/114" or r["topic"] == "WEB3-GROUPE8/042204/116" or r["topic"] == "WEB3-GROUPE8/042205/112" or r["topic"] == "WEB3-GROUPE8/042205/114" or r["topic"] == "WEB3-GROUPE8/042205/116" or r["topic"] == "WEB3-GROUPE8/042206/112" or r["topic"] == "WEB3-GROUPE8/042206/114" or r["topic"] == "WEB3-GROUPE8/042206/116" or r["topic"] == "WEB3-GROUPE8/042207/112" or r["topic"] == "WEB3-GROUPE8/042101/112" or r["topic"] == "WEB3-GROUPE8/042101/114")';
-        $tables = $client->createQueryApi()->queryStream($query, $org);
+  |> filter(fn: (r) => r["_measurement"] == "Humidité" or r["_measurement"] == "Pression" or r["_measurement"] == "Temperature")
+  |> filter(fn: (r) => r["NodeID"] == "042102" or r["NodeID"] == "042103" or r["NodeID"] == "042104" or r["NodeID"] == "042101" or r["NodeID"] == "042105" or r["NodeID"] == "042106" or r["NodeID"] == "042107" or r["NodeID"] == "042108" or r["NodeID"] == "042109" or r["NodeID"] == "042110" or r["NodeID"] == "042202" or r["NodeID"] == "042204" or r["NodeID"] == "042205" or r["NodeID"] == "042206" or r["NodeID"] == "042207" or r["NodeID"] == "042208" or r["NodeID"] == "042209" or r["NodeID"] == "042210" or r["NodeID"] == "042301" or r["NodeID"] == "042302" or r["NodeID"] == "042303" or r["NodeID"] == "042304")
+  |> filter(fn: (r) => r["_field"] == "data_value")';
+    $tables = $client->createQueryApi()->queryStream($query, $org);
 
-        foreach ($tables->each() as $record) {
-            $response[] = $record;
-        }
+    foreach ($tables->each() as $record) {
+      $record_item = $record->values;
+      $measurement = $record_item["_measurement"];
 
-        return new JsonResponse($response);
+      $response[] = [
+        "sensor_id" => SENSOR_TYPE[$measurement],
+        "type" => $measurement,
+        "node_id" => $record_item['NodeID'],
+        "value" => $record_item['_value'],
+      ];
     }
+
+    return new JsonResponse($response);
+  }
 }
