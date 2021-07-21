adfedaf75ac2371a9b1bf640cc70660f2541a260
rorrotapia <rodrigo.tapiabravo@gmail.com>
2021-07-21 09:48:33 +0200

    optimisation apifinluxcontroller

--- src/Controller/ApiInfluxController.php
+++ src/Controller/ApiInfluxController.php
@@ -9,20 +9,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\ApiInfluxData;
 
-const SENSOR_TYPE = [
-  'Temperature' => 112,
-  'Humidité' => 114,
-  'Pression' => 116
-];
-
 class ApiInfluxController extends AbstractController
 {
   /**
    * @Route("/api/influx", name="api_influx")
    */
-  public function index(): Response
+  public function index(JsonMessage $jsonMessage): Response
   {
     $response = ApiInfluxData::getRequestInfluxData();
+
+    if (!$response || empty($response)) {
+      return $jsonMessage->getEmptyDataMessage();
+    }
 
     return new JsonResponse($response);
   }
