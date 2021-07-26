7070c5949fe2a1c40c8268f1ccad9f2f03fcd51e
rorrotapia <rodrigo.tapiabravo@gmail.com>
2021-07-26 15:12:00 +0200

    add try and catch apicpostcontroller

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -21,53 +21,80 @@ class ApiPostController extends AbstractController
    */
   public function index(int $id_building, Request $request, JsonMessage $jsonMessage, EntityManagerInterface $em, IncidentRepository $incidentRepository, CompanyRepository $companyRepository): JsonResponse
   {
-    //     EXEMPLE  https://localhost:8000/api/post/agenda/new_intervention/1?date=2021-2-14&time=09:00&id_company=1&incident_ids[]=2&comment=coucou
-    $response = [];
-    $errors = [];
+    try {
+      //EXEMPLE  https://localhost:8000/api/post/agenda/new_intervention/1?date=2021-2-14&time=09:00&id_company=1&incident_ids[]=2&comment=coucou
+      $response = [];
+      $errors = [];
 
-    $params = [
-      'date',
-      'time',
-      'comment',
-      'id_company',
-      'incident_ids'
-    ];
+      $params = [
+        'date',
+        'time',
+        'comment',
+        'id_company',
+        'incident_ids'
+      ];
 
-    foreach ($params as $param) {
-      if (empty($request->query->get($param)) && $param != 'comment') {
-        $errors[] = "{$param} is empty";
+      foreach ($params as $param) {
+        if (empty($request->query->get($param)) && $param != 'comment') {
+          $errors[] = "{$param} is empty";
+        }
+        $routeParameters[$param] = $request->query->get($param);
       }
-      $routeParameters[$param] = $request->query->get($param);
-    }
 
-    $date = \DateTime::createFromFormat('Y-m-j H:i', $routeParameters['date'] . " " . $routeParameters['time']);
+      if (!empty($errors)) {
+        throw new \Exception(serialize($errors));
+      }
 
-    $company = $companyRepository->findOneBy(['id' => $routeParameters['id_company']]);
+      $date = \DateTime::createFromFormat('Y-m-j H:i', $routeParameters['date'] . " " . $routeParameters['time']);
 
-    if ($routeParameters['comment']) {
-      $newIntervention = (new Intervention())->setComment($routeParameters['comment'])->setCompany($company)->setDatetime($date);
-    } else {
-      $newIntervention = (new Intervention())->setCompany($company)->setDatetime($date);
-    }
+      if (!$date) {
+        throw new \Exception(serialize(["date not valid"]));
+      }
 
-    $em->persist($newIntervention);
-    $em->flush();
+      $company = $companyRepository->findOneBy(['id' => $routeParameters['id_company']]);
 
-    foreach ($routeParameters['incident_ids'] as $id) {
-      $updateIncident = $incidentRepository->findOneBy(['id' => $id]);
-      $updateIncident->setIntervention($newIntervention);
+      if (!isset($company)) {
+        throw new \Exception(serialize(["company don't exist"]));
+      }
 
-      $em->persist($updateIncident);
+      if ($routeParameters['comment']) {
+        $newIntervention = (new Intervention())->setComment($routeParameters['comment'])->setCompany($company)->setDatetime($date);
+      } else {
+        $newIntervention = (new Intervention())->setCompany($company)->setDatetime($date);
+      }
+
+      if (!isset($newIntervention)) {
+        throw new \Exception(serialize(["intervention don't exist"]));
+      }
+
+      $em->persist($newIntervention);
       $em->flush();
+
+      foreach ($routeParameters['incident_ids'] as $id) {
+        $updateIncident = $incidentRepository->findOneBy(['id' => $id]);
+
+        if (!isset($updateIncident)) {
+          throw new \Exception(serialize(["incident_id° {$id} don't exist"]));
+        }
+
+        $updateIncident->setIntervention($newIntervention);
+        $em->persist($updateIncident);
+        $em->flush();
+      }
+
+//      $response = new Response($response);
+//      $response->headers->set('Content-Type', 'application/json');
+
+      return new JsonResponse([
+        "success" => true,
+        "response" => $response
+      ]);
+
+    }catch (\Exception $exception) {
+      return new JsonResponse([
+        "success" => false,
+        "response" => unserialize($exception->getMessage())
+      ]);
     }
-
-    if (isset($errors)) {
-      return new JsonResponse($errors);
-    }
-
-    $response = new Response($response);
-    $response->headers->set('Content-Type', 'application/json');
-
-    return $response;
   }
 }
